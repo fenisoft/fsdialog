13fb88bb8401255ea899e5e6f8b2c7246d3fd53c
fenisoft <fenisoft@gmail.com>
2023-09-29 18:24:09 +0200

    main

---
+++ app.js
@@ -0,0 +1,81 @@
+// import { fsDialog, fsPrompt } from './lib/fsdialogs.js';
+
+//
+function simpleDialog() {
+    const buttons = [
+        { text: `<i class="bi bi-check2"></i> <b>OK</b>`, value: 'OK', class: "btn btn-success btn-sm" }
+    ];
+    const options = {
+        headClass: "bg-success  text-white",
+        closeButton: false
+    }
+    fsdialog.fsDialog(buttons, `The Force is with you, young <b class="text-primary">Skywalker</b>. 
+    But you are not a <b class="text-success">Jedi</b> yet!`, 'Simple dialog', options);
+}
+
+//
+async function confirmDialog() {
+    const result = document.querySelector("#confirmResult");
+    result.innerHTML = 'Result:';
+    const buttons = [
+        { text: `<b>YES</b>`, value: 'YES', class: "btn btn-success btn-sm" },
+        { text: `<b>NO</b>`, value: 'NO', class: "btn btn-dark btn-sm" }
+    ];
+    const options = {
+        headClass: "bg-danger text-white",
+        closeButton: true,
+    }
+
+
+    const response = await fsdialog.fsDialog(buttons, 'Are you sure ?', 'Confirm dialog', options);
+    result.innerHTML = `Result: ${response}`;
+
+}
+
+async function promptDialog() {
+    const result = document.querySelector("#promptResult");
+    const response = await fsdialog.fsPrompt(result.textContent, 'Insert value:', 'Prompt Dialog', { headClass: 'bg-warning text-white bg-gradient' });
+    if (response.button == 'OK') {
+        result.textContent = response.value;
+    }
+}
+
+
+
+globalThis.showInfo = function () {
+    const buttons = [
+        { text: `<b>CLOSE</b>`, value: 'OK', class: "btn btn-primary btn-sm" }
+    ];
+    const options = {
+        headClass: "bg-success  text-white",
+        closeButton: false,
+        container: document.querySelector("#innerDialog")
+    }
+    const html= /*html*/`<div class="font-8">
+            <b>Dante Alighieri</b>, o Alighiero, battezzato Durante di Alighiero degli Alighieri e 
+            anche noto con il solo nome di Dante, della famiglia Alighieri (Firenze, tra il 14 maggio e il 13 giugno 1265 – 
+            Ravenna, notte tra il 13 e il 14 settembre[1][2][3] 1321), è stato un poeta, scrittore e politico italiano.
+        </div>
+    `;
+    fsdialog.fsDialog(buttons, html, 'Dante Alighieri', options);
+}
+
+
+
+
+
+document.getElementById("testBtn").addEventListener('click', () => {
+    simpleDialog();
+});
+
+document.getElementById("confirmBtn").addEventListener('click', () => {
+    confirmDialog();
+})
+
+document.getElementById("promptBtn").addEventListener('click', () => {
+    promptDialog();
+})
+
+document.getElementById("innerBtn").addEventListener('click', () => {
+    document.getElementById("innerDialog").showModal();
+})
